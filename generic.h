22f2a9a37b4525288271b9e557cc615aa37582e9
Tomáš Dejmek <dejmektomas@gmail.com>
2015-04-17 19:29:20 +0200

    add doxygen comments

--- generic.h
+++ generic.h
@@ -3,52 +3,154 @@
 
 #include "tree.h"
 
-
+/**
+ * @brief Constructs a tree representing a string. The string can be either constant or not.
+ * 
+ * @param string The string to be represented.
+ * @param isConst A flag indicating if the string is a constant.
+ * @return tree The constructed tree node representing the string constant.
+ */
 tree build_string_const(const char* string, int isConst);
 
-
+/**
+ * @brief Constructs a tree representing an integer print statement.
+ * 
+ * @param loc The location of the print statement.
+ * @param int_expr The tree node representing the integer expression to be printed.
+ * @param nl Optional parameter indicating if a newline should be printed (default is true).
+ * @return tree The constructed tree node representing the print statement.
+ */
 tree build_print_int(location_t loc, tree int_expr, bool nl = true);
 
-
+/**
+ * @brief Constructs a tree representing a string print statement.
+ * 
+ * @param loc The location of the print statement.
+ * @param string The tree node representing the string to be printed.
+ * @return tree The constructed tree node representing the print statement.
+ */
 tree build_print_string(location_t loc, tree string);
 
-
+/**
+ * @brief Constructs a tree representing an exit statement.
+ * 
+ * @return tree The constructed tree node representing the exit statement.
+ */
 tree build_exit();
 
-
+/**
+ * @brief Constructs a tree representing an integer scan statement.
+ * 
+ * @param loc The location of the scan statement.
+ * @param var_expr The tree node representing the variable where the scanned integer will be stored.
+ * @return tree The constructed tree node representing the scan statement.
+ */
 tree build_scan_int(location_t loc, tree var_expr);
 
-
+/**
+ * @brief Constructs a tree representing a global variable declaration.
+ * 
+ * @param name The name of the global variable.
+ * @return tree The constructed tree node representing the global variable declaration.
+ */
 tree build_global_var(const string & name);
 
-
+/**
+ * @brief Constructs a tree representing a string wrapper.
+ * 
+ * @param name The name of the string.
+ * @param nl Optional parameter indicating if a newline should be included (default is true).
+ * @return tree The constructed tree node representing the string wrapper.
+ */
 tree build_string_wrapper(const char * name, bool nl = true);
 
-
+/**
+ * @brief Constructs a tree representing a for loop.
+ * 
+ * @param iter The tree node representing the loop iterator.
+ * @param endVal The tree node representing the end value of the loop.
+ * @param to If false, the loop counts down; otherwise, it counts up.
+ * @param statement The tree node representing the statement to be executed in each loop iteration.
+ * @return tree The constructed tree node representing the for loop.
+ */
 tree build_for(tree iter, tree endVal, bool to, tree statement);
 
-
+/**
+ * @brief Constructs a tree representing a while loop.
+ * 
+ * @param condition The tree node representing the loop condition.
+ * @param statement The tree node representing the statement to be executed while the condition is true.
+ * @param to If false, the loop counts down; otherwise, it counts up.
+ * @return tree The constructed tree node representing the while loop.
+ */
 tree build_while(tree condition, tree statement);
 
-
+/**
+ * @brief Constructs a tree representing the main function with statements.
+ * 
+ * @param stmts The tree node representing the statements in the main function.
+ * @return tree The constructed tree node representing the main function.
+ */
 tree build_main2(tree stmts);
 
-
+/**
+ * @brief Constructs a tree representing the main function without statements.
+ * 
+ * @return tree The constructed tree node representing the main function.
+ */
 tree build_main1();
 
-
+/**
+ * @brief Constructs a tree representing a function definition.
+ * 
+ * @param name The name of the function.
+ * @param params The tree node representing the function parameters.
+ * @param param_decl The tree node representing the parameter declarations.
+ * @param resdecl The tree node representing the result declaration.
+ * @param vars The tree node representing the local variables.
+ * @param stmts The tree node representing the statements in the function.
+ * @return tree The constructed tree node representing the function definition.
+ */
 tree build_function(const string & name, tree params, tree param_decl, tree resdecl, tree vars, tree stmts);
 
-
+/**
+ * @brief Constructs a tree representing a function declaration.
+ * 
+ * @param name The name of the function.
+ * @param params The tree node representing the function parameters.
+ * @param param_decl The tree node representing the parameter declarations.
+ * @param isFunc A flag indicating if the declaration is for a function.
+ * @return tree The constructed tree node representing the function declaration.
+ * 
+ * @note This function is marked as a FIXME, indicating it may require additional work or revision.
+ */
 tree build_function_dec(const string & name, tree params, tree param_decl, bool isFunc);
 
-
+/**
+ * @brief Binds a block of code to a function.
+ * 
+ * @param fndecl The tree node representing the function declaration.
+ * @param resdecl The tree node representing the result declaration.
+ * @param vars The tree node representing the local variables.
+ * @param stmts The tree node representing the statements in the function.
+ * @return tree The constructed tree node representing the bound function.
+ */
 tree bind_block_to_func(tree fndecl, tree resdecl, tree vars, tree stmts);
 
-
+/**
+ * @brief Constructs a tree representing a decrement operation.
+ * 
+ * @param n The tree node representing the variable to be decremented.
+ * @return tree The constructed tree node representing the decrement operation.
+ */
 tree build_dec(tree n);
 
-
+/**
+ * @brief Constructs a tree representing an increment operation.
+ * 
+ * @param n The tree node representing the variable to be incremented.
+ * @return tree The constructed tree node representing the increment operation.
+ */
 tree build_inc(tree n);
 
 
--- tokentypes.h
+++ tokentypes.h
@@ -4,63 +4,70 @@
 #include <string>
 
 
-
+/**
+ * @enum TokenID
+ * @brief Enumeration of token identifiers for the Mila programming language.
+ */
 enum TokenID
 {
-    KW_IF,          
-    KW_THEN,        
-    KW_BEGIN,       
-    KW_END,         
-    KW_PROCEDURE,   
-    KW_FUNCTION,    
-    KW_FOR,         
-    KW_WHILE,       
-    KW_DO,          
-    KW_TO,          
-    KW_DOWNTO,      
-    KW_CONST,       
-    KW_READLN,      
-    KW_WRITELN,     
-    KW_EXIT,        
-    KW_COLON,       
-    KW_COMMA,       
-    KW_DOT,         
-    KW_SEMICOLON,   
-    KW_DOUBLEDOT,   
-    KW_LSQBRACKET,  
-    KW_RSQBRACKET,  
-    KW_VAR,         
-    KW_OF,          
-    KW_LBRACKET,    
-    KW_RBRACKET,    
-    KW_PROGRAM,     
-    KW_INTEGER,     
-    KW_ARRAY,       
-    KW_FORWARD,     
-    KW_BREAK,       
-    KW_CONTINUE,    
-    KW_ELSE,        
-    INTEGER,        
-    OP_PLUS,        
-    OP_MINUS,       
-    OP_MUL,         
-    OP_DIV,         
-    OP_MOD,         
-    OP_ASSIGN,      
-    LO_OR,          
-    LO_AND,         
-    LO_NOT,         
-    LO_EQ,          
-    LO_NE,          
-    LO_LE,          
-    LO_GE,          
-    LO_L,           
-    LO_G,           
-    STRING,         
-    IDENTFIER       
+    KW_IF,          /**< Keyword 'if' */
+    KW_THEN,        /**< Keyword 'then' */
+    KW_BEGIN,       /**< Keyword 'begin' */
+    KW_END,         /**< Keyword 'end' */
+    KW_PROCEDURE,   /**< Keyword 'procedure' */
+    KW_FUNCTION,    /**< Keyword 'function' */
+    KW_FOR,         /**< Keyword 'for' */
+    KW_WHILE,       /**< Keyword 'while' */
+    KW_DO,          /**< Keyword 'do' */
+    KW_TO,          /**< Keyword 'to' */
+    KW_DOWNTO,      /**< Keyword 'downto' */
+    KW_CONST,       /**< Keyword 'const' */
+    KW_READLN,      /**< Keyword 'readln' */
+    KW_WRITELN,     /**< Keyword 'writeln' */
+    KW_EXIT,        /**< Keyword 'exit' */
+    KW_COLON,       /**< Symbol ':' */
+    KW_COMMA,       /**< Symbol ',' */
+    KW_DOT,         /**< Symbol '.' */
+    KW_SEMICOLON,   /**< Symbol ';' */
+    KW_DOUBLEDOT,   /**< Symbol '..' */
+    KW_LSQBRACKET,  /**< Symbol '[' */
+    KW_RSQBRACKET,  /**< Symbol ']' */
+    KW_VAR,         /**< Keyword 'var' */
+    KW_OF,          /**< Keyword 'of' */
+    KW_LBRACKET,    /**< Symbol '(' */
+    KW_RBRACKET,    /**< Symbol ')' */
+    KW_PROGRAM,     /**< Keyword 'program' */
+    KW_INTEGER,     /**< Keyword 'integer' */
+    KW_ARRAY,       /**< Keyword 'array' */
+    KW_FORWARD,     /**< Keyword 'forward' */
+    KW_BREAK,       /**< Keyword 'break' */
+    KW_CONTINUE,    /**< Keyword 'continue' */
+    KW_ELSE,        /**< Keyword 'else' */
+
+    INTEGER,        /**< Integer literal */
+    OP_PLUS,        /**< Operator '+' */
+    OP_MINUS,       /**< Operator '-' */
+    OP_MUL,         /**< Operator '*' */
+    OP_DIV,         /**< Operator '/' */
+    OP_MOD,         /**< Operator '%' */
+    OP_ASSIGN,      /**< Operator ':=' */
+    LO_OR,          /**< Logical operator 'or' */
+    LO_AND,         /**< Logical operator 'and' */
+    LO_NOT,         /**< Logical operator 'not' */
+    LO_EQ,          /**< Logical operator '=' */
+    LO_NE,          /**< Logical operator '<>' */
+    LO_LE,          /**< Logical operator '<=' */
+    LO_GE,          /**< Logical operator '>=' */
+    LO_L,           /**< Logical operator '<' */
+    LO_G,           /**< Logical operator '>' */
+    STRING,         /**< String literal */
+    IDENTFIER       /**< Identifier */
 };
 
-
+/**
+ * @var TokenNames
+ * @brief Array of token identifier names as strings.
+ */
 const char * TokenNames[] = {
     "KW_IF",
     "KW_THEN",
@@ -116,16 +123,31 @@ const char * TokenNames[] = {
     "IDENTFIER",
 };
 
-
+/**
+ * @class Token
+ * @brief Represents a token in the Mila programming language.
+ */
 class Token {
 public:
-
+    /**
+     * @brief Constructs a Token object.
+     * 
+     * @param tid The token identifier.
+     * @param data The token data (default is an empty string).
+     * @param value The token value (default is 0).
+     */
     Token(TokenID tid, const std::string & data = "", int value = 0);
 
-
+    /**
+     * @brief Destroys the Token object.
+     */
     ~Token();
 
-
+    /**
+     * @brief Prints the token information as a string.
+     * 
+     * @return std::string The token information.
+     */
     std::string print() const;
 
 public:
@@ -134,7 +156,10 @@ public:
     int mValue;          /**< The token value. */
 };
 
-
+/**
+ * @var gCurrentToken
+ * @brief Global pointer to the current token being processed.
+ */
 extern Token * gCurrentToken;
 
 #endif // TOKEN_TYPES_H
